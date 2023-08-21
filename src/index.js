1f2f9d6d78dbab5b9769b7091b2d6cc9973e7a5f
aadityaa <aditya.oza256@gmail.com>
2023-08-21 21:37:08 +0530

    added validation for location input

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './style.css'
+import './style.css';
 
+const inputField = document.querySelector('input');
 
+inputField.addEventListener('input', () => {
+    if (inputField.validity.patternMismatch) {
+        console.log('true');
+    inputField.setCustomValidity('Location should be only text.');
+    } else {
+        inputField.setCustomValidity('');
+    }
+});
