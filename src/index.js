b3baf632f160bd722a88dd7738f8bc9b3e544243
aadityyxa <aditya.oza256@gmail.com>
2023-08-21 21:50:35 +0530

    data-fetching functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css';
 
+const form = document.querySelector('form');
 const inputField = document.querySelector('input');
+const myAPIKey = '30356c336ec44fd79a9144540232108';
 
 inputField.addEventListener('input', () => {
     if (inputField.validity.patternMismatch) {
@@ -9,4 +11,16 @@ inputField.addEventListener('input', () => {
     } else {
         inputField.setCustomValidity('');
     }
+});
+
+async function fetchData() {
+    const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=${myAPIKey}&q=${inputField.value}&days=1&aqi=no&alerts=yes`);
+    const weatherData = await response.json();
+
+    console.log(weatherData);
+}
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    fetchData();
 });
